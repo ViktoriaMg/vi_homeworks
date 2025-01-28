08db0da9a13cd252d08518c0af665072517993c6
Вікторія Макогон <Viktoriia.Makohon@cs.khpi.edu.ua>
2025-01-28 17:48:36 +0200

    +

---
+++ homework_2/homework_2.3/homework_2.3.cpp
@@ -0,0 +1,10 @@
+﻿// Виправте помилку в вправі 3, після чого перекомпілюйте її, скомпонуйте і запустіть на виконання.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Is there a bug here ? " ; //двокрапка та лапки 
+	return 0;
+}
